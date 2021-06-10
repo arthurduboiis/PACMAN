a5f855e3473f1836e8bc7c00725fa241de8f9774
liamgenebrier <liam.leksir@gmail.com>
2021-06-10 13:19:47 +0200

    JavaDoc

--- src/BACK/OBJETS/ObjetMobile.java
+++ src/BACK/OBJETS/ObjetMobile.java
@@ -17,6 +17,12 @@ public class ObjetMobile extends Objet{
 
     private boolean boosted;
 
+    /**
+     * initialise objet mobile
+     * @param t typeObjet
+     * @param coordonneeX int
+     * @param coordoneeY int
+     */
     public ObjetMobile(typeObjet t, int coordonneeX, int coordoneeY) {
         super(t);
         super.setObjetMobile(this);
@@ -24,6 +30,13 @@ public class ObjetMobile extends Objet{
         this.objetSauvegarde = new ObjetDecors(typeObjet.VIDE);
     }
 
+    /**
+     * initialise objet mobile
+     * @param t typeObjet
+     * @param coordonneeX int
+     * @param coordoneeY int
+     * @param Image String
+     */
     public ObjetMobile(typeObjet t, int coordonneeX, int coordoneeY, String Image) {
         super(t);
         super.setObjetMobile(this);
@@ -36,27 +49,50 @@ public class ObjetMobile extends Objet{
 
     /////////////////////////
 
-
+    /**
+     * renvoie l'objet sauvegarde
+     * @return Objet
+     */
     public Objet getObjetSauvegarde() {
         return objetSauvegarde;
     }
 
+    /**
+     * initialise l'objet sauvegarde
+     * @param objetSauvegarde Objet
+     */
     public void setObjetSauvegarde(Objet objetSauvegarde) {
         this.objetSauvegarde = objetSauvegarde;
     }
 
+    /**
+     * verifie si boosted
+     * @return boolean
+     */
     public boolean isBoosted() {
         return boosted;
     }
 
+    /**
+     * initialise boosted
+     * @param boosted boolean
+     */
     public void setBoosted(boolean boosted) {
         this.boosted = boosted;
     }
 
+    /**
+     * initialise coordonees
+     * @param coordonees int
+     */
     public void setCoordonees(int[] coordonees) {
         this.coordonees = coordonees;
     }
 
+    /**
+     * renvoie coordonees
+     * @return int[]
+     */
     public int[] getCoordonees() {
         return coordonees;
     }
